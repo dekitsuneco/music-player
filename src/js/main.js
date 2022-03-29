2ee99f419ee19142344ca8c0ad89c66431fd6237
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-30 03:25:32 +0500

    Define constants and range mapping function

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
 // @ts-check
+
 // 1. Cache the DOM:
 /**
  * @typedef {Object} Controls
  * @property {HTMLElement} prev
  * @property {HTMLElement} play
  * @property {HTMLElement} next
+ */
+/**
+ * @typedef {Object} Progress
+ * @property {number} min
+ * @property {number} current
+ * @property {number} max
+ */
+/**
+ * @typedef {Object} Scale
+ * @property {number} min
+ * @property {number} max
  */
 
 /**
@@ -41,15 +53,41 @@ const songName = document.querySelector('.audio-meta__title');
  */
 const songCover = document.querySelector('.audio-cover__disk img');
 
-
-
-
 // 2. Define constants:
 /**
  *@todo Define a song list as an array of their titles
  */
-console.assert(songs typeof Array, 'Song list is not array');
-console.assert(songs.every(song => song typeof string), 'Song list is not array of strings');
+/**
+ * @type {Progress}
+ */
+const progress = {
+  min: 0,
+  current: 0,
+  max: 100,
+};
+
+// TODO
+/**
+ * @type {Scale}
+ */
+const widthScale = { min: 0, max: 100 };
+
+// TODO
+/**
+ * @type {Scale}
+ */
+const durationScale = { min: 0, max: 60 };
+
+/**
+ * @type {Array<string>}
+ */
+const songs = ['Pyrite', 'Pinesoot'];
+
+console.assert(songs instanceof Array, 'Song list is not array');
+console.assert(
+  songs.every((song) => typeof song === 'string'),
+  'Song list is not array of strings',
+);
 
 // 3. Define functions:
 /**
@@ -68,8 +106,30 @@ console.assert(songs.every(song => song typeof string), 'Song list is not array 
 // toNextSong(): void
 // updateProgress(e): void
 // updateProgressManually(e): void
-console.assert(mapNumBetweenRanges(fromRange, toRange, num) typeof Number,
-  'Return value of range mapping function is not array');
+/**
+ * @param {Scale} fromRange
+ * @param {Scale} toRange
+ * @param {number} num
+ * @returns {number}
+ */
+const mapNumBetweenRanges = (fromRange, toRange, num) => {
+  /**
+   * @type {number}
+   */
+  const sizeOfToRange = toRange.max - toRange.min;
+  /**
+   * @type {number}
+   */
+  const sizeOfFromRange = fromRange.max - fromRange.min;
+
+  return (
+    toRange.min + (sizeOfToRange / sizeOfFromRange) * (num - fromRange.min)
+  );
+};
+console.assert(
+  typeof mapNumBetweenRanges(widthScale, durationScale, 30) === 'number',
+  'Return value of range mapping function is not array',
+);
 
 // 4. Set event listeners:
 /**
